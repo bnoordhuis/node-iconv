d4fe13e14f5c0377ad2b68f356a7cc738d04a472
Ben Noordhuis <info@bnoordhuis.nl>
2010-07-12 23:33:35 +0200

    node.js iconv(3) bindings, first iteration.

---
+++ iconv.cc
@@ -0,0 +1,137 @@
+#include <iconv.h>
+
+#include <v8.h>
+#include <node.h>
+#include <node_buffer.h>
+
+#include <cstring>
+#include <cerrno>
+#include <cstdio>
+
+using namespace v8;
+using namespace node;
+
+namespace {
+
+class Iconv: public ObjectWrap {
+public:
+  static void Initialize(Handle<Object>& target);
+  static Handle<Value> New(const Arguments& args);
+  static Handle<Value> Convert(const Arguments& args);
+
+  Iconv(iconv_t conv);
+  ~Iconv(); // destructor may not run if program is short-lived or aborted
+
+  // the actual conversion happens here
+  Handle<Value> Convert(char* data, size_t length);
+
+private:
+  iconv_t conv_;
+};
+
+Iconv::Iconv(iconv_t conv): conv_(conv) {
+  assert(conv_ != (iconv_t) -1);
+}
+
+Iconv::~Iconv() {
+  iconv_close(conv_);
+}
+
+// the actual conversion happens here
+Handle<Value> Iconv::Convert(char* data, size_t length) {
+  assert(conv_ != (iconv_t) -1);
+  assert(data != 0);
+
+  char *inbuf;
+  char *outbuf;
+  size_t inbytesleft;
+  size_t outbytesleft;
+
+  inbuf = data;
+  inbytesleft = length;
+
+  char tmp[1024];
+  outbuf = tmp;
+  outbytesleft = sizeof(tmp);
+
+  size_t n = iconv(conv_, &inbuf, &inbytesleft, &outbuf, &outbytesleft);
+  if (n == (size_t) -1) {
+    const char* message = "Unexpected error.";
+
+    switch (errno) {
+    case E2BIG:
+      message = "Output buffer not large enough. This is a bug.";
+      break;
+    case EILSEQ:
+      message = "Illegal character sequence.";
+      break;
+    case EINVAL:
+      message = "Incomplete character sequence.";
+      break;
+    }
+
+    return ThrowException(ErrnoException(errno, "iconv", message));
+  }
+
+  n = sizeof(tmp) - outbytesleft;
+  Buffer& b = *Buffer::New(n);
+  memcpy(b.data(), tmp, n);
+
+  return b.handle_;
+}
+
+Handle<Value> Iconv::Convert(const Arguments& args) {
+  HandleScope scope;
+
+  Iconv* self = ObjectWrap::Unwrap<Iconv>(args.This());
+  Local<Value> arg = args[0];
+
+  if (arg->IsString()) {
+    String::Utf8Value string(arg->ToString());
+    return self->Convert(*string, string.length());
+  }
+
+  if (arg->IsObject()) {
+    Handle<Value> object = arg->ToObject();
+    if (Buffer::HasInstance(object)) {
+      Buffer& buffer = *ObjectWrap::Unwrap<Buffer>(arg->ToObject());
+      return self->Convert(buffer.data(), buffer.length());
+    }
+  }
+
+  return Undefined();
+}
+
+Handle<Value> Iconv::New(const Arguments& args) {
+  HandleScope scope;
+
+  String::AsciiValue sourceEncoding(args[0]->ToString());
+  String::AsciiValue targetEncoding(args[1]->ToString());
+
+  iconv_t conv = iconv_open(*targetEncoding, *sourceEncoding);
+  if (conv == (iconv_t) -1) {
+    return ThrowException(ErrnoException(errno, "iconv_open", "Conversion not supported."));
+  }
+
+  Iconv* instance = new Iconv(conv);
+  instance->Wrap(args.Holder());
+
+  return args.This();
+}
+
+void Iconv::Initialize(Handle<Object>& target) {
+  HandleScope scope;
+
+  Local<FunctionTemplate> t = FunctionTemplate::New(Iconv::New);
+  t->InstanceTemplate()->SetInternalFieldCount(1);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "convert", Iconv::Convert);
+
+  target->Set(String::NewSymbol("Iconv"), t->GetFunction());
+}
+
+extern "C" void init(Handle<Object> target) {
+  Iconv::Initialize(target);
+}
+
+} // namespace
---
+++ test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+var Buffer = require('buffer').Buffer;
+var Iconv = require('./iconv').Iconv;
+
+// unknown source/target encoding
+assert.throws(function() { new Iconv('utf-8', 'xxx'); });
+assert.throws(function() { new Iconv('xxx', 'utf-8'); });
+
+var iconv = new Iconv('utf-8', 'iso-8859-1');
+assert.ok(undefined === iconv.convert());
+assert.ok(undefined === iconv.convert(1));
+assert.ok(undefined === iconv.convert({}));
+
+assert.ok(iconv.convert('xxx') instanceof Buffer);
+assert.ok(iconv.convert(new Buffer('xxx')) instanceof Buffer);
+
+assert.ok(new Buffer('xxx').inspect() == iconv.convert('xxx').inspect());
+assert.ok(new Buffer('xxx').inspect() == iconv.convert(new Buffer('xxx')).inspect());
+
+var buffer = new Buffer(1); buffer[0] = 235; // ë
+assert.ok(buffer.inspect() == iconv.convert('ë').inspect());
+
+// partial character sequence - throws for now
+assert.throws(function() {
+	buffer = new Buffer(1); buffer[0] = 195;
+	iconv.convert(buffer);
+});
+
+// belongs to partial character sequence test - new input should be recoded without issues
+buffer = new Buffer(1); buffer[0] = 235; // ë
+assert.ok(buffer.inspect() == iconv.convert('ë').inspect());
