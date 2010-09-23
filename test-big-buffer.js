a2ff96002abee95ed282a36bef14d8b56e232520
Ben Noordhuis <info@bnoordhuis.nl>
2010-09-23 23:25:19 +0200

    Fix for test failure: encoding to UTF-16 is implementation dependent, output may be either big or little endian. Therefore, encode explicitly to UTF-16LE.

--- test-big-buffer.js
+++ test-big-buffer.js
@@ -1,7 +1,7 @@
 var Iconv = require('iconv').Iconv,
    assert = require('assert');
 
-var iconv = new Iconv('UTF-8', 'UTF-16');
+var iconv = new Iconv('UTF-8', 'UTF-16LE');
 
 var utf8 = new Buffer(20000000);
 for (var i = 0; i < utf8.length; i++) {
@@ -9,15 +9,10 @@ for (var i = 0; i < utf8.length; i++) {
 }
 var utf16 = iconv.convert(utf8);
 
-// 2 bytes per character + 2 bytes for BOM
-assert.equal(utf16.length, utf8.length * 2 + 2);
-
-// test for BOM
-assert.equal(utf16[0], 0xff);
-assert.equal(utf16[1], 0xfe);
+assert.equal(utf16.length, utf8.length * 2);
 
 for (i = 0; i < utf8.length; ++i) {
-  assert.equal(utf16[2 + i * 2], utf8[i]);
-  assert.equal(utf16[2 + i * 2 + 1], 0);
+  assert.equal(utf16[i * 2], utf8[i]);
+  assert.equal(utf16[i * 2 + 1], 0);
 }
 
