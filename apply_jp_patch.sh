6f189a7f537a278986e4f342c175d3bfeaccdf07
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-08-13 06:19:59 +0900

    シェル修正

--- apply_jp_patch.sh
+++ apply_jp_patch.sh
@@ -1,5 +1,6 @@
 cd ./deps/libiconv-1.13.1/;
 gzip -dc ../libiconv-1.13-ja-1.patch.gz | patch -p1;
+rm -f ../libiconv-1.13-ja-1.patch.gz;
 ./configure --disable-shared --enable-static --enable-relocatable --enable-extra-encodings;
 make;
 cd ../../;
