1ea0a2257c3c93c77c072fdaf3d5e5cdd96f49c6
Kazuhisa Hara <kazuhisya@gmail.com>
2011-08-23 18:01:38 +0900

    remove the patche-script and processed within the wscript.

--- apply_jp_patch.sh
+++
@@ -1,7 +0,0 @@
-cd ./deps/libiconv-1.13.1/;
-gzip -dc ../libiconv-1.13-ja-1.patch.gz | patch -p1;
-rm -f ../libiconv-1.13-ja-1.patch.gz;
-./configure --disable-shared --enable-static --enable-relocatable --enable-extra-encodings;
-make;
-cd ../../;
-make;
