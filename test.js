ed1a8f235b9249fd529f9752027b535e3dc1c4f9
David Bonet Montes <webbonet@gmail.com>
2016-03-01 11:30:35 +0100

    Added more info on tesperformance test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var Iconv = require('./lib/iconv.js').Iconv;
 var iconvDE = new Iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', 'de_DE.UTF-8');
 var iconvEN = new Iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', 'en_US.UTF-8');
 
-var ITERATIONS = 10000;
+var ITERATIONS = 100000;
 var TEST_STRING = 'ü  ä  ö  ß  Ü  Ä  Ö {"·45 g kjshdflkj hl sññplñlñl+çñç';
 var GERMAN_TRANSLITERATION = 'ue  ae  oe  ss  UE  AE  OE {"?45 g kjshdflkj hl snnplnlnl+cnc';
 var ENGLISH_TRANSLITERATION = 'u  a  o  ss  U  A  O {"?45 g kjshdflkj hl snnplnlnl+cnc';
@@ -14,7 +14,10 @@ for(var i = ITERATIONS; i >= 0; i--){
 	iconvDE.convert(TEST_STRING).toString();
 }
 
-console.log('Time for ' + ITERATIONS + ' iterations with ' + TEST_STRING.length + ' characters string: ' + (Date.now() - time) + 'ms');
+var elapsedTime = (Date.now() - time);
+
+console.log('Time for ' + ITERATIONS + ' iterations with ' + TEST_STRING.length + ' characters string: ' + elapsedTime + 'ms');
+console.log('One transliteration takes: ' + (elapsedTime / ITERATIONS).toFixed(3) + "ms");
 
 if(testDE() && testC() && testC() && testDE() && testDE() && testDE() && testC() && testDE())
 	console.log('All test OK');
